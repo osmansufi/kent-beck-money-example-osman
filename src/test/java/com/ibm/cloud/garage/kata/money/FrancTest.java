fa09e7e7ab81dd95d00f1bc65037433ca2386d17
Osman Sufi <osufi@ford.com>
2021-12-17 16:49:27 -0500

    create franc class by copying dollar

---
+++ src/test/java/com/ibm/cloud/garage/kata/money/FrancTest.java
@@ -0,0 +1,14 @@
+package com.ibm.cloud.garage.kata.money;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FrancTest {
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+}
